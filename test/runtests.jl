4b9322f785edf3c39e0a630d3914bd784e5372e0
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2018-03-23 08:18:21 -0400

    Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,19 +2,19 @@ using RemarkableInts
 using Test
 
 ordinary5  = 5
-@test !marked(ordinary5)       # with ordinary Ints (UInts)
+@test !ismarked(ordinary5)       # with ordinary Ints (UInts)
 @unmark!(ordinary5)            # @mark!, @unmark! are allowed
 @mark!(ordinary5)              # (to simplify mixed use)
-@test !marked(ordinary5)       # they do nothing
+@test !ismarked(ordinary5)       # they do nothing
 
 markable5  = MInt(5)
-@test !marked(markable5)
+@test !ismarked(markable5)
 @mark!(markable5)              # markable Ints can be marked
-@test marked(markable5)        # 
+@test Ismarked(markable5)        # 
 @unmark!(markable5)            # and unmarked
-@test !marked(markable5)       # 
+@test !ismarked(markable5)       # 
 @mark!(markable5)              # and remarked
-@test marked(markable5)        # 
+@test ismarked(markable5)        # 
 
 
 # marked and unmarked vars of equal value measure equal
@@ -22,8 +22,8 @@ markable5  = MInt(5)
 markable5  = MUInt(5)
 unmarked5  = markable5
 @mark!(markable5)
-@test marked(markable5)
-@test !marked(unmarked5)
+@test ismarked(markable5)
+@test !ismarked(unmarked5)
 @test markable5 == unmarked5
 
 # indexing       1  2  3  4  5  6   7   8   9
