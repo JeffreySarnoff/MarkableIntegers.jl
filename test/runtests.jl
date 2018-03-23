82d794cd8dc6e8a7ff3d97b32e49b5d22381c21e
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2018-03-23 07:29:32 -0400

    Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,12 +1,41 @@
 using RemarkableIntegers
 using Test
 
-ivalue1 = 5
-ivalue2 = 9999
-inote1  = NoteInt(ivalue1)
-inote2  = NoteInt(2)
+ordinary5  = 5
+@test !marked(ordinary5)       # with ordinary Ints (UInts)
+@unmark!(ordinary5)            # @mark!, @unmark! are allowed
+@mark!(ordinary5)              # (to simplify mixed use)
+@test !marked(ordinary5)       # they do nothing
 
-@test isnoted(ivalue1)  == false
-@test isnoted(ivalue2)  == false
-@test isnoted(inote1)  == false
-@test isnoted(inote2)  == false
+markable5  = MInt(5)
+@test !marked(markable5)
+@mark!(markable5)              # markable Ints can be marked
+@test marked(markable5)        # 
+@unmark!(markable5)            # and unmarked
+@test !marked(markable5)       # 
+@mark!(markable5)              # and remarked
+@test marked(markable5)        # 
+
+
+# marked and unmarked vars of equal value measure equal
+
+markable5  = MUInt(5)
+unmarked5  = markable5
+@mark!(markable5)
+@test marked(markable5)
+@test !marked(unmarked5)
+@test markable5 == unmarked5
+
+# indexing       1  2  3  4  5  6   7   8   9
+vec = Markable.([1, 3, 5, 7, 3, 9, 11, 11, 15])
+@test !any(marked, vec)
+
+@mark!(vec[5])
+@mark!(vec[7])
+@mark!(vec[8])
+
+@test any(marked, vec)
+@test findall(marked, vec) = [3, 7, 8]
+@test map(marked, vec) = [5, 11, 11]
+
+
