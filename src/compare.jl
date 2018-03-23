f4d13e5c08aea33b07dd9a5dd047d4bae6e078d0
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2018-03-23 07:57:10 -0400

    Update compare.jl

--- src/compare.jl
+++ src/compare.jl
@@ -1,12 +1,13 @@
 for OP in (:(==), :(!=), :(<=), :(>=), :(<), :(>))
     @eval begin
-        $OP(a::T, b::T) where {T<:NoteSigned}   = $OP(signed(a), signed(b))
-        $OP(a::T, b::T) where {T<:NoteUnsigned} = $OP(unsigned(a), unsigned(b))
+        $OP(a::T, b::T) where {T<:MarkSigned}   = $OP(signed(a), signed(b))
+        $OP(a::T, b::T) where {T<:MarkUnsigned} = $OP(unsigned(a), unsigned(b))
     end
 end
 
-isless(a::T, b::T)  where{T<:NoteSigned} = signed(a) <  signed(b)
-isequal(a::T, b::T) where{T<:NoteSigned} = signed(a) == signed(b)
+isless(a::T, b::T)  where{T<:MarkSigned} = signed(a) <  signed(b)
+isequal(a::T, b::T) where{T<:MarkSigned} = signed(a) == signed(b)
 
-isless(a::T, b::T)  where{T<:NoteUnsigned} = unsigned(a) <  unsigned(b)
-isequal(a::T, b::T) where{T<:NoteUnsigned} = unsigned(a) == unsigned(b)
+isless(a::T, b::T)  where{T<:MarkUnsigned} = unsigned(a) <  unsigned(b)
+isequal(a::T, b::T) where{T<:MarkUnsigned} = unsigned(a) == unsigned(b)
+
