d524b34f4fa24a5805753af0c46be5d1c4de0047
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2018-03-29 16:55:40 -0400

    Update compare.jl

--- src/compare.jl
+++ src/compare.jl
@@ -1,21 +1,21 @@
 for OP in (:(==), :(!=), :(<=), :(>=), :(<), :(>))
     @eval begin
         function $OP(a::T, b::T) where {T<:MarkableInteger}
-            ia = ityped(a)
-            ib = ityped(b)
+            ia = ityped(a) >> 1
+            ib = ityped(b) >> 1
             return $OP(ia, ib)
         end
         function $OP(a::T1, b::T2) where {T1<:MarkableInteger, T2<:MarkableInteger}
-            ia = ityped(a)
-            ib = ityped(b)
+            ia = ityped(a) >> 1
+            ib = ityped(b) >> 1
             return $OP(ia, ib)
         end
         function $OP(a::T1, b::T2) where {T1<:MarkableInteger, T2<:Union{Signed,Unsigned}}
-            ia = ityped(a)
+            ia = ityped(a) >> 1
             return $OP(ia, b)
         end
         function $OP(a::T1, b::T2) where {T2<:MarkableInteger, T1<:Union{Signed,Unsigned}}
-            ib = ityped(b)
+            ib = ityped(b) >> 1
             return $OP(a, ib)
         end
     end
