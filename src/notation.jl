eb5fb8426c672b14e521dd442081f0a1d718a2e1
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2018-03-29 14:04:57 -0400

    Update notation.jl

--- src/notation.jl
+++ src/notation.jl
@@ -40,10 +40,10 @@ for (M,I) in ((:MarkInt128, :Int128), (:MarkInt64, :Int64),
   end
 end
 
-findall(x::Vector{T}) where {T<:MarkedIntegers}  = findall(map(ismarked, x))
+findall(x::Vector{T}) where {T<:MarkableIntegers}  = findall(map(ismarked, x))
 findallnot(x) = map((!),findall(x))
-allmarked(x::Vector{T}) where {T<:MarkedIntegers}   = x[findall(x)]
-allunmarked(x::Vector{T}) where {T<:MarkkedIntegers} = x[findallnot(x)]
+allmarked(x::Vector{T}) where {T<:MarkableIntegers}   = x[findall(x)]
+allunmarked(x::Vector{T}) where {T<:MarkableIntegers} = x[findallnot(x)]
 
 
 """
