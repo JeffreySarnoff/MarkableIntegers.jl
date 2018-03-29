cfbfb495d26f8538fe5552865eace48efa815fef
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2018-03-29 13:28:05 -0400

    Update notation.jl

--- src/notation.jl
+++ src/notation.jl
@@ -1,52 +1,21 @@
-mark(x::MInt128) = (|)(x, lsbit(MInt128))
-mark(x::MInt64)  = (|)(x, lsbit(MInt64))
-mark(x::MInt32)  = (|)(x, lsbit(MInt32))
-mark(x::MInt16)  = (|)(x, lsbit(MInt16))
-mark(x::MInt8)   = (|)(x, lsbit(MInt8))
 
-mark(x::MUInt128) = (|)(x, lsbit(MUInt128))
-mark(x::MUInt64)  = (|)(x, lsbit(MUInt64))
-mark(x::MUInt32)  = (|)(x, lsbit(MUInt32))
-mark(x::MUInt16)  = (|)(x, lsbit(MUInt16))
-mark(x::MUInt8)   = (|)(x, lsbit(MUInt8))
+for (M,I) in ((:MarkInt128, :Int128), (:MarkInt64, :Int64), 
+              (:MarkInt32, :Int32), (:MarkInt16, :Int16), (:MarkInt8, :Int8),
+              (:MarkUInt128, :UInt128), (:MarkUInt64, :UInt64), 
+              (:MarkUInt32, :UInt32), (:MarkUInt16, :UInt16), (:MarkUInt8, :UInt8))
+  @eval begin
+    ismarked(x::$M) = isodd(reinterpret($I, x))
+    isunmarked(x::$M) = iseven(reinterpret($I, x))
+    ismarked(x::$I) = false
+    isunmarked(x::$I) = true
+    mark(x::$M) = reinterpret($M, reinterpret($I,x) | lsbit($I))
+    unmark(x::$M) = reinterpret($M, reinterpret($I,x) & msbits($I))
+    mark(x::$I) = Marked(x)
+    unmark(x::$I) = Unmarked(x)
+  end
+end
 
-mark(x::T) where {T<:Signed}   = x
-mark(x::T) where {T<:Unsigned} = x
-
-
-unmark(x::MInt128) = (&)(x, msbits(MInt128))
-unmark(x::MInt64)  = (&)(x, msbits(MInt64))
-unmark(x::MInt32)  = (&)(x, msbits(MInt32))
-unmark(x::MInt16)  = (&)(x, msbits(MInt16))
-unmark(x::MInt8)   = (&)(x, msbits(MInt8))
-
-unmark(x::MUInt128) = (&)(x, msbits(MUInt128))
-unmark(x::MUInt64)  = (&)(x, msbits(MUInt64))
-unmark(x::MUInt32)  = (&)(x, msbits(MUInt32))
-unmark(x::MUInt16)  = (&)(x, msbits(MUInt16))
-unmark(x::MUInt8)   = (&)(x, msbits(MUInt8))
-
-unmark(x::T) where {T<:Signed}   = x
-unmark(x::T) where {T<:Unsigned} = x
-
-
-ismarked(x::MInt128) = (&)(x, lsbit(MInt128)) === lsbit(MInt128)
-ismarked(x::MInt64)  = (&)(x, lsbit(MInt64))  === lsbit(MInt64)
-ismarked(x::MInt32)  = (&)(x, lsbit(MInt32))  === lsbit(MInt32)
-ismarked(x::MInt16)  = (&)(x, lsbit(MInt16))  === lsbit(MInt16)
-ismarked(x::MInt8)   = (&)(x, lsbit(MInt8))   === lsbit(MInt8)
-
-ismarked(x::MUInt128) = (&)(x, lsbit(MUInt128)) === lsbit(MUInt128)
-ismarked(x::MUInt64)  = (&)(x, lsbit(MUInt64))  === lsbit(MUInt64)
-ismarked(x::MUInt32)  = (&)(x, lsbit(MUInt32))  === lsbit(MUInt32)
-ismarked(x::MUInt16)  = (&)(x, lsbit(MUInt16))  === lsbit(MUInt16)
-ismarked(x::MUInt8)   = (&)(x, lsbit(MUInt8))   === lsbit(MUInt8)
-
-ismarked(x::T) where {T<:Signed}   = false
-ismarked(x::T) where {T<:Unsigned} = false
-
-@inline marked(x::Vector{T}) where {T<:MIntegers} = map(ismarked, x)
-findall(x::Vector{T}) where {T<:MIntegers}  = findall(marked(x))
+findall(x::Vector{T}) where {T<:MarkedIntegers}  = findall(map(ismarked, x))
 findallnot(x) = map((!),findall(x))
-allmarked(x::Vector{T}) where {T<:MIntegers}   = x[findall(x)]
-allunmarked(x::Vector{T}) where {T<:MIntegers} = x[findallnot(x)]
+allmarked(x::Vector{T}) where {T<:MarkedIntegers}   = x[findall(x)]
+allunmarked(x::Vector{T}) where {T<:MarkkedIntegers} = x[findallnot(x)]
